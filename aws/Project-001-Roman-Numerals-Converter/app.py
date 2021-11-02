03e0d85120f8eecab3a9fb668af3c881e51c371d
JohnWhyter <87023137+JohnWhyter@users.noreply.github.com>
2021-11-02 22:13:27 +0300

    updated 2

--- aws/Project-001-Roman-Numerals-Converter/app.py
+++ aws/Project-001-Roman-Numerals-Converter/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, render_template
 
-from num_to_roman import int_to_rome
-
 app = Flask(__name__)
+
+def int_to_rome(number): 
+           
+        n = int(number)
+        b = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+        c = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+        d= []
+        for i in range(len(b)):
+            count = int((n/b[i]))
+            d.append(count*c[i])
+            n -= count * b[i]
+        return "".join(d)
 
 
 @app.route("/", methods=["GET", "POST"])
